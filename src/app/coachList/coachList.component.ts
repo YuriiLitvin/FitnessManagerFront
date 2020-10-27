bc4beaefe853d4723d32a67bdc5c8feb78c7c601
yurijlitviniii@gmail.com <yurijlitviniii@gmail.com>
2020-10-27 16:04:46 +0200

    field "coaches" added, li tag implemented

--- src/app/coachList/coachList.component.ts
+++ src/app/coachList/coachList.component.ts
@@ -6,18 +6,50 @@ import {Component} from '@angular/core';
     styleUrls: ['./coachList.component.scss']
 })
 export class CoachListComponent {
-    title = 'My Coach Title'
+    title = 'coaches'
     
-
-    getCoaches(){
-        const apiLink = "https://localhost:5001/api/Coach"
-
-        const coachRequest = fetch(apiLink)
-        
-        coachRequest
-        .then((response: { json: () => any; })=>response.json())
-        .then((coahes: any)=>console.table(coahes))
+       coaches = [{
+            "firstName": "Petrovich",
+            "lastName": "",
+            "email": "petrovich@gmail.com",
+            "mobileNumber": "09923",
+            "typeOfTraining": 1
+        },
+        {
+            "firstName": "Samson",
+            "lastName": "",
+            "email": "samson@gmail.com",
+            "mobileNumber": "097325",
+            "typeOfTraining": 2
+        },
+        {
+            "firstName": "Oleksandr",
+            "lastName": "I",
+            "email": "sashkapower@gmail.com",
+            "mobileNumber": "09544234",
+            "typeOfTraining": 3
+        },
+        {
+            "firstName": "Anna",
+            "lastName": "G.",
+            "email": "g_anna@gmail.com",
+            "mobileNumber": "0930954",
+            "typeOfTraining": 0
+        },
+        {
+            "firstName": "John",
+            "lastName": "True",
+            "email": "trueJohn@urk.net",
+            "mobileNumber": "06623453",
+            "typeOfTraining": 3
+        },
+        {
+            "firstName": "Billy",
+            "lastName": "Moodily",
+            "email": "dugMoodug@gmail.com",
+            "mobileNumber": "00044434",
+            "typeOfTraining": 2
+        }]
     
-    }
 
 }
